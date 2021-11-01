0418e17882f0f4100b5a81fd832167df09049102
jash56 <64904906+jash56@users.noreply.github.com>
2021-11-01 08:57:33 -0400

    Time: 992 ms (87.75%), Space: 25 MB (43.12%) - LeetHub

--- finding-the-users-active-minutes/finding-the-users-active-minutes.py
+++ finding-the-users-active-minutes/finding-the-users-active-minutes.py
@@ -1,16 +1,15 @@
+from collections import defaultdict
 class Solution:
     def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
         
-        answer = [0] * k  #list of number of users whose uam is j
-        user_uam = {} #hash table for each user and corresponding minute list for which they are active
+        user_uam = defaultdict(set) #hash table for each user and corresponding minute set for which they are active
         
-        #populate hash table for each user and their active minute list
+        #populate hash table for each user and their active minute set
         for user_id, time in logs:  
-            active_min_list = user_uam.get(user_id, [])
-            if time not in active_min_list:
-                active_min_list.append(time)
-            user_uam[user_id] = active_min_list
+            user_uam[user_id].add(time)
 
+        answer = [0] * k  #list of number of users whose uam is j
+        
         #iterate over the active minute list for each user and populate answer list
         for value in user_uam.values():
             answer[len(value)-1] += 1
