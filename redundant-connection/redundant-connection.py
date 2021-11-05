0a92977f507fbcc97dd7f8c22a0354a6f2656129
jash56 <64904906+jash56@users.noreply.github.com>
2021-11-05 18:20:28 -0400

    Time: 52 ms (92.43%), Space: 15 MB (42.06%) - LeetHub

--- redundant-connection/redundant-connection.py
+++ redundant-connection/redundant-connection.py
@@ -1,35 +1,34 @@
 class Solution:
     
-    def create_vertex_list(self, edges):
-        vertex_list = []
-        for u, v in edges:
-            if u not in vertex_list:
-                vertex_list.append(u)
-            if v not in vertex_list:
-                vertex_list.append(v)
-        return vertex_list
-    
+    # function to find parent/representative/root of given node
     def find(self, parent_list, node):
-        node = node - 1
         parent = parent_list[node]
-        while parent != -1:
-            node = parent
-            parent = parent_list[node]      
-        return node
+        while parent != parent_list[parent]:
+            parent = parent_list[parent]
+        return parent
             
+    # function to combine 2 subsets based on the rank of their roots        
+    def union(self, parent_list, u_representative, v_representative):
+        # u has higher rank than v
+        if parent_list[u_representative] < parent_list[v_representative]:
+            #parent_list[u_representative] += parent_list[v_representative]
+            parent_list[v_representative] = u_representative
+        else:
+            #parent_list[v_representative] += parent_list[u_representative]
+            parent_list[u_representative] = v_representative
+        return parent_list
+        
+    # function to find the edge of cycle removing which will make a tree
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        
-        parent_list = [-1] * len(self.create_vertex_list(edges))
-        
-        for u, v in edges:
-            
-            u_representative = self.find(parent_list, u)
-            v_representative = self.find(parent_list, v)
-            
-            if u_representative == v_representative:
+        parent_list = [i for i in range((len(edges) + 1))]
+        rank = [1] * len(parent_list)
+        for u, v in edges:  
+            u_root = self.find(parent_list, u)
+            v_root = self.find(parent_list, v)
+            if u_root == v_root:
                 return [u, v]
             else:
-                parent_list[u_representative] = v_representative
+                self.union(parent_list, u_root, v_root)
                 
             
         
