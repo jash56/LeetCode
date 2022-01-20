580a97793b85c9e3e68c158c31b09559d7fe8a94
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-20 05:54:11 -0500

    Time: 869 ms (40.46%), Space: 28.8 MB (11.66%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -5,6 +5,5 @@ class Solution:
         curr_sum = 0
         for i in nums:
             curr_sum = max(i, curr_sum+i)
-            print(curr_sum, i)
             max_sum = max(max_sum, curr_sum)
         return max_sum
