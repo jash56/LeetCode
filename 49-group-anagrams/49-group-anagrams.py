d5235c6b1dfb81e6a3cc3d31dde83fe2c284b735
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-21 07:33:33 -0500

    Time: 188 ms (15.58%), Space: 20 MB (9.88%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,9 +1,16 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        
+        
         hashmap = defaultdict(list)
         for word in strs:
-            hashmap[tuple(sorted(list(word)))].append(word)
+            temp = [0] * 26
+            count_dict = Counter(word)
+            for key, val in count_dict.items():
+                temp[ord(key)-ord('a')] = val
+            
+            hashmap[tuple(temp)].append(word)
         return hashmap.values()
             
