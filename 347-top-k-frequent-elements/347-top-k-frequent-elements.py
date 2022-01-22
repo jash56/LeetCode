8daf098690fff501bfe9964cc2a6fab238d3c548
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-22 07:32:46 -0500

    Time: 154 ms (29.16%), Space: 18.5 MB (95.55%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        counter = collections.Counter(nums)
+        
+        count_list = list(counter.items())
+        count_list.sort(key=lambda x: x[1], reverse=True)
+        
+        print(count_list)
+        
+        ans = []
+        for i in count_list[:k]:
+            ans.append(i[0])
+        return ans
