599ece7b8aa78a6227c85a138ec8264ff79b6bf8
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-24 08:22:42 -0500

    Time: 115 ms (30.51%), Space: 14.3 MB (54.91%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,86 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        max_len = curr_len = start = 0
-        # char_index = {}
-        for end in range(len(s)):
-            if s[end] not in s[start:end]:
-                curr_len += 1
-                # print(s[end])
-            else:
-                print(start, s[start:end+1], end)
-                max_len = max(curr_len, max_len)
-                start = s.index(s[end], start, end) + 1
-                curr_len = len(s[start:end+1])
-        max_len = max(curr_len, max_len)
+        max_len = start = 0
+        char_index = {}
+        
+        for end in range(len(s)):            
+            if s[end] in char_index:
+                start = max(char_index[s[end]] + 1, start)
+        
+            char_index[s[end]] = end
+            max_len = max(end - start + 1, max_len)
                           
-        return max_len
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # max_length = curr_length = 0
-        # ptr = 0
-        # for i in range(len(s)):
-        #     if s[i] not in s[ptr:i]:
-        #         curr_length += 1
-        #         print(curr_length)
-        #     else:
-        #         max_length = max(max_length, curr_length)
-        #         ptr = s.index(s[i], ptr)
-        #         curr_length = len(s[ptr:i+1])
-        # max_length = max(max_length, curr_length)
-        # return max_length
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return max_len  
+     
         
 #         max_len = start = end = 0
 #         while end < len(s):
