dde9636547bf23fab9523e5e24cda7cb822a8a51
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-25 12:47:59 -0500

    Time: 364 ms (41.46%), Space: 25.8 MB (53.83%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -6,13 +6,11 @@ class Solution:
         nums.sort()
         longest_len = curr_len = 1
         i = 0
-        print(nums)
+
         while i < len(nums)-1:
             if nums[i] + 1 != nums[i+1]:
                 longest_len = max(longest_len, curr_len)
                 curr_len = 0
-                # i -= 1
-                print(longest_len)
 
             i += 1
             curr_len += 1 
