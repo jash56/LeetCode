03ab6e840651db4d650daadab6edeb9b41c4b04d
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-25 08:53:40 -0500

    Time: 1621 ms (5.01%), Space: 16.1 MB (41.56%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,24 +1,35 @@
 import collections
 class Solution:
-    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+    def make_graph(self, edges):
         graph = collections.defaultdict(list)
-        for x, y in edges:
-            graph[x].append(y)
-            graph[y].append(x)
+        for edge in edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
         
-        def bfs(node, seen):
-            queue = collections.deque([node])
-            while queue:
-                node = queue.popleft()
-                for neighbor in graph[node]:
-                    if neighbor not in seen:
-                        seen.add(neighbor)
-                        queue.append(neighbor)
-        count = 0
-        seen = set()
-        for node in range(n):
-            if node not in seen:
-                seen.add(node)
-                bfs(node, seen)
-                count += 1
-        return count
+        return graph
+            
+    def bfs(self, graph, start):
+        
+        queue = [start]
+        visited = []
+        while queue:
+            current = queue.pop(0)
+            visited.append(current)
+            for neigh in graph[current]:
+                if neigh not in visited:
+                    queue.append(neigh)
+        return visited
+            
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        graph = self.make_graph(edges)
+        print(graph)
+        component_count = 0
+        visited = set()
+        for vertex in range(n):
+            if vertex not in visited:
+                visited.update(self.bfs(graph, vertex))
+                print(visited)
+                component_count += 1
+        return component_count
+        
