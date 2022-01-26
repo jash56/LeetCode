a2b8bd1977509063ab9de044b624361667a67264
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-26 07:40:28 -0500

    Time: 103 ms (8.95%), Space: 19.6 MB (11.90%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,12 +3,9 @@ class Solution:
         
         input_list = []
         for i in s:
-            if ord(i) >= ord('a') and ord(i) <= ord('z'):
-                input_list.append(i)
-            elif ord(i) >= ord('A') and ord(i) <= ord('Z'):
+            if i.isalnum():
                 input_list.append(i.lower())
-            elif ord('0') <= ord(i) <= ord('9'):
-                input_list.append(i)
+            
         s = ''.join(input_list)
         
         start, end = 0, len(s) - 1
