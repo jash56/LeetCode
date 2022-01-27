24d7fe7081d18f09502cd19b8d1fe8f2636c649d
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 08:11:30 -0500

    Time: 227 ms (20.16%), Space: 14.3 MB (54.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,17 +1,52 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        max_len = start = 0
-        char_index = {}
+        if len(s) == 0 or len(s) == 1: return len(s)
         
-        for end in range(len(s)):            
-            if s[end] in char_index:
-                start = max(start, char_index[s[end]] + 1)
+        start = 0
+        longest_len = 1
 
-            char_index[s[end]] = end
-            max_len = max(end - start + 1, max_len)
+        for end in range(1, len(s)):
+            print(start, end, longest_len)
+            if s[end] in s[start:end]:
+                longest_len = max(end - start, longest_len)
+                start = s.index(s[end], start, end) + 1
+                
+        return max(end - start + 1, longest_len)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         max_len = start = 0
+#         char_index = {}
+        
+#         for end in range(len(s)):            
+#             if s[end] in char_index:
+#                 start = max(start, char_index[s[end]] + 1)
+
+#             char_index[s[end]] = end
+#             max_len = max(end - start + 1, max_len)
                           
-        return max_len  
+#         return max_len  
      
         
 #         max_len = start = end = 0
