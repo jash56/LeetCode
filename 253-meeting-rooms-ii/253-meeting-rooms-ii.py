eebbff00fd2f90eee8bee06f61e23fc10abe3b34
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 11:31:46 -0500

    Time: 88 ms (50.66%), Space: 17.7 MB (25.18%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,20 @@
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        intervals.sort(key=lambda x: x[0])
+        
+        in_use = []
+        heapq.heappush(in_use, intervals[0][1])
+        room_count = 1
+        
+        for interval in intervals[1:]:
+            print(in_use)
+            if interval[0] >= in_use[0]:
+                heapq.heappop(in_use)
+                heapq.heappush(in_use, interval[1])
+            else:
+                heapq.heappush(in_use, interval[1])
+                room_count += 1
+                
+        return room_count
