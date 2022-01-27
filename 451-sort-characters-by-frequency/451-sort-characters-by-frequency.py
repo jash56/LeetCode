12617ec2387c920ea4792a77e574509f205cf762
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 16:34:14 -0500

    Time: 113 ms (9.89%), Space: 15.3 MB (98.64%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -4,6 +4,32 @@ class Solution:
         counter = {}
         for i in s:
             counter[i] = counter.get(i, 0) + 1
+        
+        counter = [[val, key] for key, val in counter.items()]
+        counter.sort(reverse=True)
+        ans = [char * freq for freq, char in counter]
+        return ''.join(ans)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         counter_list = list(counter.items())
         counter_list.sort(key=lambda x: x[1], reverse=True)
         string_builder = []
