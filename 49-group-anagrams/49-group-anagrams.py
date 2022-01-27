3b612174596e5b5b201ba3c2839e65989d2b1534
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 11:53:31 -0500

    Time: 116 ms (57.28%), Space: 20.1 MB (9.97%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,15 +2,46 @@ from collections import defaultdict, Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        anagram_match = collections.defaultdict(list)       
+        ans = []
+        for string in strs:
+            string_freq = [0] * 26
+            for char in string:
+                string_freq[ord(char)-ord('a')] += 1 
+            anagram_match[tuple(string_freq)].append(string)
+        
+        for group in anagram_match.values():
+            ans.append(list(group))
+        return ans
         
         
-        hashmap = defaultdict(list)
-        for word in strs:
-            temp = [0] * 26
-            count_dict = Counter(word)
-            for key, val in count_dict.items():
-                temp[ord(key)-ord('a')] = val
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         hashmap = defaultdict(list)
+#         for word in strs:
+#             temp = [0] * 26
+#             count_dict = Counter(word)
+#             for key, val in count_dict.items():
+#                 temp[ord(key)-ord('a')] = val
             
-            hashmap[tuple(temp)].append(word)
-        return hashmap.values()
+#             hashmap[tuple(temp)].append(word)
+#         return hashmap.values()
             
