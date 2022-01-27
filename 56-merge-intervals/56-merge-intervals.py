3c6d4514dde0f72c93d118c6574a299accc0aec5
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 11:01:27 -0500

    Time: 276 ms (7.18%), Space: 18 MB (40.15%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,16 +1,46 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        if len(intervals) == 1: return intervals
-        intervals = sorted(intervals, key=lambda x: x[0])
-        
-        final_intervals = [intervals[0]]
-
-        for interval in intervals:
+        intervals.sort(key=lambda x: x[0])
+        ans = [intervals[0]]
+        for interval in intervals[1:]:
             
-            if interval[0] <= final_intervals[-1][1]:
-                final_intervals[-1][1] = max(interval[1], final_intervals[-1][-1])
+            if ans[-1][1] >= interval[0]:
+                ans[-1][1] = max(ans[-1][1], interval[1])
             else:
-                final_intervals.append(interval)
+                ans.append(interval)
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(intervals) == 1: return intervals
+#         intervals = sorted(intervals, key=lambda x: x[0])
+        
+#         final_intervals = [intervals[0]]
+
+#         for interval in intervals:
+            
+#             if interval[0] <= final_intervals[-1][1]:
+#                 final_intervals[-1][1] = max(interval[1], final_intervals[-1][-1])
+#             else:
+#                 final_intervals.append(interval)
                 
-        return final_intervals
+#         return final_intervals
