d6d2e5dd8a605767a8892b3d5fa904ac51cb9ad3
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 16:18:25 -0500

    Time: 590 ms (5.01%), Space: 22.3 MB (22.93%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,16 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        pre = [1] * (len(nums))
-        post = [1] * (len(nums))
-        for i in range(1, len(nums)):
-            print(i)
-            pre[i] = pre[i-1] * nums[i-1]
+#         pre = [1] * (len(nums))
+#         post = [1] * (len(nums))
+#         for i in range(1, len(nums)):
+#             pre[i] = pre[i-1] * nums[i-1]        
+#         for i in range(len(nums)-2, -1, -1):
+#             post[i] = post[i+1] * nums[i+1]
         
-        for i in range(len(nums)-2, -1, -1):
-            post[i] = post[i+1] * nums[i+1]
-        
-        return [pre[i] * post[i] for i in range(len(nums))]
+#         return [pre[i] * post[i] for i in range(len(nums))]
         
         
         
@@ -35,10 +33,9 @@ class Solution:
         
         
         n = len(nums)
-        prefix, postfix, answer = [1] * n, [1] * n, [0] * n
+        prefix, postfix = [1] * n, [1] * n
         for i in range(1, n):
             prefix[i] = prefix[i-1] * nums[i-1]
             postfix[n-i-1] = postfix[n-i] * nums[n-i]
-        for i in range(n):
-            answer[i] = prefix[i] * postfix[i]
-        return answer
+    
+        return [prefix[i] * postfix[i] for i in range(len(nums))]
