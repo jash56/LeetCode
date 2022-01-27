b7032c50a6a80a8287b6b1898b4c2bf3168e37cf
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 07:53:01 -0500

    Time: 60 ms (21.87%), Space: 14.1 MB (85.80%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,18 +3,60 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        j = m - 1
-        k = n - 1
-        for i in range(n + m - 1, -1, -1):
-            if k < 0:
-                break
-            if j >= 0 and nums1[j] > nums2[k]:
-                nums1[i] = nums1[j]
-                j -= 1
-                
+        
+        
+        i = len(nums1)
+        m -= 1
+        n -= 1
+        while m >= 0 and n >= 0:
+            print(i, nums1)
+            i -= 1
+            
+            if nums1[m] < nums2[n]:
+                nums1[i] = nums2[n]
+                n -= 1
             else:
-                nums1[i] = nums2[k]
-                k -= 1
+                nums1[i] = nums1[m]
+                m -= 1
+        
+        while n >= 0:
+            i -= 1
+            nums1[i] = nums2[n]
+            n -= 1
+            
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # j = m - 1
+        # k = n - 1
+        # for i in range(n + m - 1, -1, -1):
+        #     if k < 0:
+        #         break
+        #     if j >= 0 and nums1[j] > nums2[k]:
+        #         nums1[i] = nums1[j]
+        #         j -= 1          
+        #     else:
+        #         nums1[i] = nums2[k]
+        #         k -= 1
             
         
             
