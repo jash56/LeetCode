5b1c661a9c4180ccc982ec711a5cf4e01daec0ee
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-26 20:55:23 -0500

    Time: 3476 ms (22.99%), Space: 18.6 MB (11.45%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -4,22 +4,23 @@ class Solution:
         nums.sort()
         ans = set()
         for i in range(len(nums)):
-            start, end = i+1, len(nums)-1
-            while start < end:
-                temp = nums[start] + nums[end] + nums[i]
-                if temp == 0:
-                    ans.add((nums[i], nums[start], nums[end]))
-                    if nums[start] == nums[start+1]:
-                        start += 1
-                    elif nums[end] == nums[end-1]:
+            if nums[i] <= 0:
+                start, end = i+1, len(nums)-1
+                while start < end:
+                    temp = nums[start] + nums[end] + nums[i]
+                    if temp == 0:
+                        ans.add((nums[i], nums[start], nums[end]))
+                        if nums[start] == nums[start+1]:
+                            start += 1
+                        elif nums[end] == nums[end-1]:
+                            end -= 1
+                        else:
+                            start += 1
+                            end -= 1
+                    elif temp > 0:
                         end -= 1
                     else:
                         start += 1
-                        end -= 1
-                elif temp > 0:
-                    end -= 1
-                else:
-                    start += 1
             
         return [list(x) for x in ans]
                     
