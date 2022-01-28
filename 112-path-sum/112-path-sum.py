277f9e19f64df7c3fa23e60b428ffac61cef9018
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-27 19:18:48 -0500

    Time: 40 ms (88.87%), Space: 15.1 MB (46.29%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -6,18 +6,27 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        #rec dfs
         
         if not root: return False
-        queue = [(root, root.val)]
         
-        while queue:
-            curr, curr_sum = queue.pop(-1)  
-            if not curr.left and not curr.right and curr_sum == targetSum:
-                return True
-            if curr.left:
-                queue.append((curr.left, curr_sum + curr.left.val)) 
-            if curr.right:
-                queue.append((curr.right, curr_sum + curr.right.val))
+        if not root.left and not root.right and targetSum == root.val:
+            return True
+        
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+        
+#bfs
+#         if not root: return False
+#         queue = [(root, root.val)]
+        
+#         while queue:
+#             curr, curr_sum = queue.pop(-1)  
+#             if not curr.left and not curr.right and curr_sum == targetSum:
+#                 return True
+#             if curr.left:
+#                 queue.append((curr.left, curr_sum + curr.left.val)) 
+#             if curr.right:
+#                 queue.append((curr.right, curr_sum + curr.right.val))
             
-        return False
+#         return False
                 
