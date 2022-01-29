b3b0674aacb5a6959b00d77d125e1f4b7090b74a
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-29 06:35:56 -0500

    Time: 144 ms (69.45%), Space: 13.8 MB (99.85%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,24 +1,59 @@
 class Solution:
-    
-    def is_palindrome(self, s):
-        
-        start, end = 0, len(s)-1
-        while start < end:
-            if s[start] != s[end]:
-                return False
-            start += 1
-            end -= 1
-        return True
-    
-    def countSubstrings(self, s: str) -> int:
-        ans = 0
-        valid = set()
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                if s[i:j+1] not in valid:
-                    if self.is_palindrome(s[i:j+1]):
-                        ans += 1
-                        valid.add(s[i:j+1])
-                else:
-                    ans += 1
+    def countSubstrings(self, s: str) -> int:        
+        ans = 1
+        for i in range(1, len(s)):          
+            ans += self.palindrome_from_center(s, i, i)
+            ans += self.palindrome_from_center(s, i-1, i)
         return ans
+    
+    def palindrome_from_center(self, s, left, right):
+        count = 0
+        while left >= 0 and right < len(s):
+            
+            if s[left] == s[right]:
+                count += 1
+            else:
+                break
+            left -= 1
+            right += 1
+        return count
+                
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # def is_palindrome(self, s):
+        
+#         start, end = 0, len(s)-1
+#         while start < end:
+#             if s[start] != s[end]:
+#                 return False
+#         return True
+    
+#     def countSubstrings(self, s: str) -> int:
+#         ans = 0
+#         valid = set()
+#         for i in range(len(s)):
+#             for j in range(i, len(s)):
+#                 if self.is_palindrome(valid, s[i:j+1]):
+#                     ans += 1
+#         return ans
