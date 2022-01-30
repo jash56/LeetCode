8d725f414339c7f2b7bf0881dc98181a44ec9dea
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-30 12:16:21 -0500

    Time: 56 ms (48.00%), Space: 15.6 MB (62.74%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -6,40 +6,48 @@
 #         self.right = right
 class Solution:
     
-    def rec(self, root, path, ans):
+    def rec(self, root, path, ans, targetSum):
         
-        if not root:
-            return ans
+        if not root: return 
+        path.append(root.val)
+        if not root.left and not root.right and root.val == targetSum:
+            ans.append(list(path))
+        else:          
+            self.rec(root.left, path, ans, targetSum-root.val)
+            self.rec(root.right, path, ans, targetSum-root.val)
+        
+        path.pop()
         
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        if not root: return []
-        stack = [(root, [root.val])]
         ans = []
-        while stack:
-            curr, curr_path = stack.pop(-1)
-            if not curr.left and not curr.right and sum(curr_path) == targetSum:
-                ans.append(curr_path)          
-            
-            if curr.right:     
-                stack.append((curr.right, curr_path + [curr.right.val]))
-                
-            if curr.left: 
-                stack.append((curr.left, curr_path + [curr.left.val]))
+        
+        self.rec(root, [], ans, targetSum)  
         return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if not root: return []
+        # stack = [(root, [root.val])]
+        # ans = []
+        # while stack:
+        #     curr, curr_path = stack.pop(-1)
+        #     if not curr.left and not curr.right and sum(curr_path) == targetSum:
+        #         ans.append(curr_path)          
+        #     if curr.left: 
+        #         stack.append((curr.left, curr_path + [curr.left.val]))
+        #     if curr.right:     
+        #         stack.append((curr.right, curr_path + [curr.right.val]))
+        # return ans
