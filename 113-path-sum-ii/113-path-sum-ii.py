2d77e5b6f57a1ee8ebdef84e10bbd7fc60a751d5
jash56 <64904906+jash56@users.noreply.github.com>
2022-01-30 11:38:57 -0500

    Time: 70 ms (29.71%), Space: 15.1 MB (95.18%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -5,7 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    
+    def rec(self, root, path, ans):
+        
+        if not root:
+            return ans
+        
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         if not root: return []
         stack = [(root, [root.val])]
@@ -14,8 +36,10 @@ class Solution:
             curr, curr_path = stack.pop(-1)
             if not curr.left and not curr.right and sum(curr_path) == targetSum:
                 ans.append(curr_path)          
-            if curr.left: 
-                stack.append((curr.left, curr_path + [curr.left.val]))
+            
             if curr.right:     
                 stack.append((curr.right, curr_path + [curr.right.val]))
+                
+            if curr.left: 
+                stack.append((curr.left, curr_path + [curr.left.val]))
         return ans
