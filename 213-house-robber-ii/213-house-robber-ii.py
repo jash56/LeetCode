d844f92c844cf8cef6ec5323f1faf1249bc1e0f9
jash56 <64904906+jash56@users.noreply.github.com>
2022-02-03 07:03:14 -0500

    Time: 55 ms (20.04%), Space: 13.9 MB (98.04%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -2,14 +2,14 @@ class Solution:
     def get_loot(self, house_list):
         
         next_house = house_list[-1]
-        next_next_house = 0
+        next_next_house = 0        
         for i in range(len(house_list)-2, -1, -1):           
-            curr = max(house_list[i]+next_next_house, next_house)            
-            next_house, next_next_house = curr, next_house 
-            
+            curr = max(house_list[i]+next_next_house, next_house)         
+            next_house, next_next_house = curr, next_house       
         return curr
         
     def rob(self, nums: List[int]) -> int:
+        
         if len(nums) <= 2:
             return max(nums)
         return max(self.get_loot(nums[:-1]), self.get_loot(nums[1:]))
