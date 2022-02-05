13132c8bab2834465354bc40174791e4900d6a58
jash56 <64904906+jash56@users.noreply.github.com>
2022-02-05 06:42:56 -0500

    Time: 84 ms (77.22%), Space: 18.7 MB (85.56%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,24 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:  
+from collections import deque
+class Solution:
         
     def rec(self, subtree_inorder_start, subtree_inorder_end, preorder, inorder_index_map):
         
         if subtree_inorder_start > subtree_inorder_end:
             return        
-        
-        root = TreeNode(val=preorder.pop(0))    
+        root = TreeNode(val=preorder.popleft())
         root.left = self.rec(subtree_inorder_start, inorder_index_map[root.val]-1, preorder, inorder_index_map)
         root.right = self.rec(inorder_index_map[root.val]+1, subtree_inorder_end, preorder, inorder_index_map)       
         return root
         
         
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-                
+               
         inorder_index_map = {} 
         for idx, val in enumerate(inorder):            
             inorder_index_map[val] = idx
-                  
+        preorder = deque(preorder)          
         return self.rec(0, len(inorder)-1, preorder, inorder_index_map)
             
