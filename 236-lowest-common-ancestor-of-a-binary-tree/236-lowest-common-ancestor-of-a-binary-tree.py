1d068092d0a59be37edadb26f7f76a36409ac2e3
jash56 <64904906+jash56@users.noreply.github.com>
2022-02-09 05:46:08 -0500

    Time: 90 ms (54.48%), Space: 26.3 MB (43.32%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -5,20 +5,18 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    def traverse(self, root, p, q, found, path):
+class Solution:       
         
+    def traverse(self, root, p, q, found, path):
         if not root:
             return
-        path.append(root)
-        
+        path.append(root)    
         if root == p:           
             found.append(list(path))     
         elif root == q:
             found.append(list(path))        
         if len(found) == 2:
-            return    
-        
+            return            
         self.traverse(root.left, p, q, found, path)
         self.traverse(root.right, p, q, found, path)
         path.pop()      
@@ -26,16 +24,12 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         found = []
         path = []
-        self.traverse(root, p, q, found, path)
-        
+        self.traverse(root, p, q, found, path)        
         idx = 0
-        n = min(len(found[0]), len(found[1]))
-        
+        n = min(len(found[0]), len(found[1]))        
         while idx < n:
             if found[0][idx].val == found[1][idx].val:
                 idx += 1  
             else:
                 break
         return found[0][idx-1]
-        
-        
