152c710258f5f2aa82b6296f5409876c95d9a890
jash56 <64904906+jash56@users.noreply.github.com>
2022-04-05 09:15:08 -0400

    Time: 3792 ms (25.00%), Space: 196.9 MB (25.00%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.scala
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.scala
@@ -3,15 +3,11 @@ object Solution {
         if (original.length != (m*n)) {
             Array.ofDim[Int](0, 0)
         }
-        else{
+        else {
             val ans = Array.ofDim[Int](m, n)
-            var idx = 0
-            for (i <- 0 to m-1) {
-                for (j <- 0 to n-1) {
-                    ans(i)(j) = original(idx) 
-                    idx = idx + 1
-                }
-            }
+            for (i <- 0 to original.length-1) {
+                println(i/m, i%n, i);
+                ans(i/n)(i%n) = original(i) }
             ans
         }   
     }
